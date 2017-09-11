5d67f68c3a2acad8bf1c7c297cb9226b84d61410
monine <bob.zhouxiang@gmail.com>
2017-09-11 11:44:05 +0800

    add base file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'cross-menu.js',
+    library: 'CrossMenu',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      // https://github.com/babel/babel-loader
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  }
+}
